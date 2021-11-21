f83de852ad3a22f2829897f33da0534b13fcc49b
Neo Maralit <neomaralit@gmail.com>
2021-11-21 03:10:43 -0500

    Added button with functions

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (name) => {
+        console.log('hello' + name);
+    }
+
+    const handleClick2 = (e) => {
+        console.log('hello', e);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={(e) => handleClick('John Smith', e)}>Click me</button>
+            <button onClick={handleClick2}>Click 2</button>
         </div>
      );
 }

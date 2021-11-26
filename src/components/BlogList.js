5ee61e22af54031c49e61607686b825636a56763
Neo Maralit <neomaralit@gmail.com>
2021-11-25 22:51:44 -0500

    Added useEffect

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title}) => {
+const BlogList = ({ blogs, title, handleDelete}) => {
     // Will create variables from parent data of props
 
     // const blogs = props.blogs;
@@ -11,6 +11,7 @@ const BlogList = ({ blogs, title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by: { blog.author }</p>
+                    <button onClick={() => handleDelete(blog.id)}>Delete Post</button>
                 </div>
             ))}
         </div>

3b7f8389dca149d5eca3aa783a8c6353569695ed
Neo Maralit <neomaralit@gmail.com>
2021-11-27 02:26:41 -0500

    Added loading useState

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setisLoading] = useState(true);
 
       useEffect(() => {
           fetch('https://localhost:8000/blogs')
@@ -12,11 +13,13 @@ const Home = () => {
             })
             .then (data => {
                 console.log(data);
+                setisLoading(false);
             })
       }, []);
 
     return ( 
         <div className="home">
+            { isLoading && <div>Loading . . . </div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
      );

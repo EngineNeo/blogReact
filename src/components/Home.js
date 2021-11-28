35f09d7941257e24f44703117a38af8575cd88d8
Neo Maralit <neomaralit@gmail.com>
2021-11-28 04:07:09 -0500

    Added error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,32 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setisLoading] = useState(true);
+    const [error, setError] = useState(null);
 
+
+    //start jsonserver
+    //npx json-server --watch data/db.json --port 8000
       useEffect(() => {
           fetch('https://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('Data was unable to be fetched')
+                }
                 return res.json();
             })
             .then (data => {
                 console.log(data);
                 setisLoading(false);
             })
+            .catch (err => {
+                setisLoading(false);
+                setError(err.message)
+            })
       }, []);
 
     return ( 
         <div className="home">
+            { error && <div> { error } </div>}
             { isLoading && <div>Loading . . . </div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>

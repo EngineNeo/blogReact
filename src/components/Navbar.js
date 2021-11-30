e086ea09942cc5a5adeef8aa59544539265521b2
Neo Maralit <neomaralit@gmail.com>
2021-11-29 19:24:27 -0500

    Added create page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>The Blog React</h1>
+            <h1>blogReact</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
